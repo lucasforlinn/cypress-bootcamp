87da964a0682d1bf942648e4a1d02e8db3367fd6
Lucas Forlin <lucasforlinn@gmail.com>
2022-09-05 11:55:21 -0300

    Corrigido as mensagens de aviso que estavam erradas. E automatizado os casos de criação de foodtruck duplicado e campos obrigatórios

--- tests/cypress/e2e/create.cy.js
+++ tests/cypress/e2e/create.cy.js
@@ -30,12 +30,55 @@ describe('Recomendação', () => {
     })
 
     it('não deve cadastrar foodtruck com o nome duplicado', ()=> {
-        // 1 - a massa de testes deve ser independente
-        // 2 - latitude e longitude deve ser única
-        // 3 - você deve encontrar e corrigir o bug
+        const user = {
+            name: 'Ed',
+            instagram: '@edhgh',
+            password: 'pwa123'
+        }
+
+        const foodtruck = {
+            latitude: '-26.916444922066976',
+            longitude: '-49.10482048988343',
+            name: 'Trailer do BBzinho',
+            details: 'Canetas mais fiéis do mercado',
+            opening_hours: 'das 8h às 20h',
+            open_on_weekends: false
+        }
+
+        //Cadastrando usuário e foodtruck via API pra ganhar tempo de execução
+        cy.apiCreateUser(user)
+        cy.apiLogin(user)
+        cy.apiCreateFoodTruck(foodtruck)
+
+
+        cy.uiLogin(user)
+        mapPage.createLink()
+        createPage.form(foodtruck)
+        createPage.submit()
+        createPage.modal.haveText('Esse food truck já foi cadastrado!')
     })
 
     it('todos os campos são obrigatórios', ()=> {
+        const user = {
+            name: 'Growth',
+            instagram: '@gsuplementos',
+            password: 'pwd123'
+        }
+
+        const foodtruck = {
+            latitude: '-23.584548837854058',
+            longitude: '-46.674446913517876',
+        }
+
+        cy.apiCreateUser(user)
+        cy.uiLogin(user)
+
+        mapPage.createLink()
+        cy.setGeolocation(foodtruck.latitude, foodtruck.longitude)
+        createPage.submit()
+
+        const message = 'Os campos nome, descrição e horário de funcionamento devem ser informados para recomendar um food truck!'
+        createPage.modal.haveText(message)
 
     })
 
